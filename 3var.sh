0d7ecb7bb84a252f1fef06518130243338ea8b35
hari charan <haricharan610@gmail.com>
2026-07-19 14:24:26 +0530

    3var.sh

--- 3var.sh
+++ 3var.sh
@@ -1,13 +1,13 @@
-#!bin/bash
+#!/bin/bash
 
 
 person1=Dr.morgon
 person2=ethan 
 
 echo "$person1: you known this place"
-echo "$person2: what's that boss
+echo "$person2: what's that boss"
 echo "$person1: This palce makes me wonder"
 echo "$person2: Yeah what that boss. "
-echo "$person1 which would be worse " TO LIVE AS A MONSTER OR TO DIE AS A GOOD MAN  "
+echo "$person1 which would be worse? TO LIVE AS A MONSTER OR TO DIE AS A GOOD MAN?  "
 echo "$person2: Tomorrow"
 echo "$person1: thats's an unsual answer"
