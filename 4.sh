55a901d5eff2e3f3baef3c2a6d5a280b6c6d6161
hari charan <haricharan610@gmail.com>
2026-07-19 14:38:22 +0530

    4.sh

--- 4.sh
+++ 4.sh
@@ -1,9 +1,9 @@
 #1/bin/bash
 
-echo " enter your pin number ::"
+echo "enter your pin number ::"
 
 read -s pin # here pin is the variable
 
-echo "your number is: pin"
+echo "your number is: $pin"
 
 
