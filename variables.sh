0bd7e29ae11a8798b0ebed2408fc90c9e6d50939
hari charan <haricharan610@gmail.com>
2026-07-19 13:26:37 +0530

    variables.sh

---
+++ variables.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+person1=  Dr. morgon
+person2=  ethan 
+
+echo "Dr. morgan: u said you've seen this room before"
+echo "ethan: i have. "
+echo " Dr.morgan "when" "
+echo "ethan: Tomorrow"
+echo "Dr. morgan: thats's an unsual answer"
