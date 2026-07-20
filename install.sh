3c5efcd25a634abc20f7c2f7e18df450b8d275a9
hari charan <haricharan610@gmail.com>
2026-07-20 20:11:14 +0530

    install.sh

--- install.sh
+++ install.sh
@@ -1,35 +1,38 @@
 #!/bin/bash
 
-userid=$( userid -ne 0 )
+userid=$(id -u)
 
 if [ $userid -ne 0 ]
 then
-exit 1
 
 echo "ERROR :: please run with root user"
 
 else
 
 echo " running with root user"
+exit 1
 fi
 
-dnf list installed cataluge 
+dnf list installed catalouge 
 
 #check if already installed or not. if installed $? 0,
 #if not installed $? is not 0.
 
-if [ $? -eq 0 ]
+if [ $? -ne 0 ]
 then
 echo "catalouge is not installed... going to install"
 dnf install cataluge -y
 
-if [$? -eq 0 ]
+if [ $? -ne 0 ]
 then
 
 echo "catalouge installing... succes"
 else
 
 echo "catalouge installing... fail"
+exit 1
+
 fi
 
-echo "catalouge installing... nothing to do"
+echo "catalouge already installed... nothing to do"
+fi 
