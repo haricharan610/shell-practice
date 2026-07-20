1f19cf274071dc8fae054c0a217aa736886dc876
hari charan <haricharan610@gmail.com>
2026-07-20 21:59:43 +0530

    3condition.sh

---
+++ 3condition.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+userid=$(id -u)
+
+if [ userid -ne 0 ]
+
+then
+
+echo "please run with root access"
+
+exit 1 
+
+else
+
+echo "you are running with root user"
+
+fi
+
+dnf list insatlled nginx 
+
+#check if any installed or not 
+#if installed $? is 0
+#if not installed $? is not 0
+
+if [ $? -ne 0 ]
+
+then 
+
+echo "nginx not installed... going to install"
+
+dnf install nginx -y
+
+if [ $? -eq 0 ]
+
+then
+
+echo "nginx installing... sucess"
+
+else 
+
+echo "nginx installing... fail"
+
+exit 1
+
+fi
+
+echo " if nginx already installed... nothing to do"
+
+fi
