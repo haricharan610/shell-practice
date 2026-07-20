69d090a639c808fa3661ff9b7d8fda89df786569
hari charan <haricharan610@gmail.com>
2026-07-20 15:32:22 +0530

    conditions.sh

--- conditions.sh
+++ conditions.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-number=$9
+number=$50
 
-if [ $number -lt 10 ]
+if [ $number -lt 100 ]
   then
-echo "given number $number is less than 10"
+echo "given number $number is less than 100"
 
   else 
 
-  echo "given number $number is greater than 10"
+  echo "given number $number is greater than 100"
   
   fi
-  
