edc908facd3dd6715eb07f506051024a81746bde
hari charan <haricharan610@gmail.com>
2026-07-20 15:14:41 +0530

    speical varibales.sh

--- speical varibales.sh
+++ speical varibales.sh
@@ -4,3 +4,16 @@ echo "All variables passed to the script: $@"
 
 echo "number of variables: $#"
 
+echo "script name: $0"
+
+echo "current directory: $PWD"
+
+echo "user running this script: $USER"
+
+echo "home directory of user: $HOME"
+
+echo "PID of the script: $$"
+
+sleep 10 &
+
+echo "PID of last command in background: $!"
