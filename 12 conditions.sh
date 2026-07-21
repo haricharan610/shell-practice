d60b8740f48b091a4a408822626f6664149128a3
hari charan <haricharan610@gmail.com>
2026-07-21 22:08:01 +0530

    12 conditions.sh

--- 12 conditions.sh
+++ 12 conditions.sh
@@ -9,61 +9,60 @@ echo "ERROR :: please run with root access"
 exit 1
 
 else
+
 echo "you are running with root access"
 fi
 
-validate(){
-    if [ $1 -eq 0 ]
+checkstatus(){
+
+if [ $1 -eq 0 ]
 then
 
 echo "installing $2... success"
 else
 
-echo "installing $2... fail"
+echo "installing $2.... fail"
 exit 1
 
 fi
 }
 
-dnf list installed mysql
+dnf list installed tree 
 
 if [ $? -ne 0 ]
 then
 
-echo "mysql not installed going to instal it"
-dfn install mysql -y
-validate $? "mysql"
+echo "tree not installed going to install it"
+dnf install tree -y
+checkstatus $? "tree"
 
 else
 
-echo "mysql already installed nothing to do"
-
+echo "tree already installed nothing to do"
 fi
 
-dnf list installed python3
+dnf list installed git
 
 if [ $? -ne 0 ]
 then
 
-echo "python3 not installed going to install it"
-dnf install python3 -y
-validate $? "python"
+echo "git not installed going to install it"
+dnf install git -y
+checkstatus $? "git"
 
 else
 
-echo "ptyhon3 already installed nothing to do"
+echo "git already installed nothing to do"
 fi
 
-dnf list installed nginx 
-
+dnf list installed nginx
 if [ $? -ne 0 ]
 then
 
 echo "nginx not installed going to install it"
-dnf install nginx -y 
-validate $? "nginx"
+dnf install nginx -y
+checkstatus $? "nginx"
 
 else
-
 echo "nginx already installed nothing to do"
 fi
