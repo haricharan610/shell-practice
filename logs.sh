67fcac92106029904f46721cec06a57187d329c3
hari charan <haricharan610@gmail.com>
2026-07-22 17:48:02 +0530

    logs.sh

--- logs.sh
+++ logs.sh
@@ -10,7 +10,7 @@ LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
-mkdir -p $ LOGS_FOLDER
+mkdir -p "$LOGS_FOLDER"
 echo "script started excueting at: $(date)" | tee -a $LOG_FILE
 
 if [ $userid -ne 0 ]
